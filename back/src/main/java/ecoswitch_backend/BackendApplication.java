952fb21fb9a1795d4782852a21c8a16e647c7b83
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-04-30 20:55:32 -0500

    fix: Configuración CORS Front v1

--- back/src/main/java/ecoswitch_backend/BackendApplication.java
+++ back/src/main/java/ecoswitch_backend/BackendApplication.java
@@ -1,13 +1,33 @@
 package ecoswitch_backend;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        String uri = System.getenv("MONGODB_URI");
+        
+        if (uri == null || uri.isEmpty()) {
+            throw new RuntimeException("La variable de entorno MONGODB_URI no está configurada");
+        }
+        
+        return MongoClients.create(uri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "EcoSwich");
     }
 }
